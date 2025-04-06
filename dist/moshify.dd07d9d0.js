b391dc87b829edd7f2454023f225e34f1fa48333
Israel Pedro <pedroisrael980@gmail.com>
2025-04-06 11:18:17 +0100

    installed svgo.

---
+++ dist/moshify.dd07d9d0.js
@@ -0,0 +1,2 @@
+let collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>{l.addEventListener("click",()=>{l.classList.toggle("collapsible--expanded")})});
+//# sourceMappingURL=moshify.dd07d9d0.js.map
