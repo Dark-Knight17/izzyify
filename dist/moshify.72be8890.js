139de2917c472ce50468a8b033a741d076357dd1
Israel Pedro <pedroisrael980@gmail.com>
2025-04-06 11:09:36 +0100

    Setup project for deployment

---
+++ dist/moshify.72be8890.js
@@ -0,0 +1,8 @@
+const collapsibles = document.querySelectorAll('.collapsible');
+collapsibles.forEach((collapsible)=>{
+    collapsible.addEventListener('click', ()=>{
+        collapsible.classList.toggle('collapsible--expanded');
+    });
+});
+
+//# sourceMappingURL=moshify.72be8890.js.map
